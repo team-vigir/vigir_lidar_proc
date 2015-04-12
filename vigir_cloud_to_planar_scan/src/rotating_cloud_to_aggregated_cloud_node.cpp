28eea70c0c5b8a7b4a8af759e843b9f95fcd26ee
kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-04-12 10:40:31 +0200

    Renaming and cleanup of rotating cloud to aggregate cloud code

--- vigir_cloud_to_planar_scan/src/rotating_cloud_to_aggregated_cloud_node.cpp
+++ vigir_cloud_to_planar_scan/src/rotating_cloud_to_aggregated_cloud_node.cpp
@@ -38,49 +38,34 @@
 
 #include <pcl_ros/transforms.h>
 
-//#include <filters/filter_chain.h>
-
-class LaserscanToPointcloud
+/**
+ * Subscribes to rotating LIDAR clouds and publishes and asssembles
+ * aggregated clouds 
+ */
+class RotatingCloudToAggregatedCloud
 {
 public:
 
-  LaserscanToPointcloud()
+  RotatingCloudToAggregatedCloud()
   {
     ros::NodeHandle nh_;
 
     prior_roll_angle_ = 0.0;
 
-    scan_sub_ = nh_.subscribe("cloud", 10, &LaserscanToPointcloud::scanCallback, this);
+    scan_sub_ = nh_.subscribe("cloud", 10, &RotatingCloudToAggregatedCloud::cloudCallback, this);
     point_cloud2_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("cloud_out",10,false);
 
     ros::NodeHandle pnh_("~");
-    //pnh_.param("max_range", p_max_range_, 29.0);
-    //pnh_.param("min_range", p_min_range_, 0.0);
 
-    //filter_chain_.configure("scan_filter_chain", pnh_);
 
-    pnh_.param("use_high_fidelity_projection", p_use_high_fidelity_projection_, true);
+    pnh_.param("target_frame", p_target_frame_, std::string("base_link"));
 
-    if (p_use_high_fidelity_projection_){
-      pnh_.param("target_frame", p_target_frame_, std::string("NO_TARGET_FRAME_SPECIFIED"));
-
-      if (p_target_frame_ == "NO_TARGET_FRAME_SPECIFIED"){
-        ROS_ERROR("No target frame specified! Needs to be set for high fidelity projection to work");
-        p_use_high_fidelity_projection_ = false;
-        return;
-      }
-
-      tfl_.reset(new tf::TransformListener());
-      wait_duration_ = ros::Duration(0.5);
-
-    }
+    tfl_.reset(new tf::TransformListener());
+    wait_duration_ = ros::Duration(0.5);
   }
 
-  void scanCallback (const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
+  void cloudCallback (const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
   {
-
-    //ros::Time scan_time = cloud_in->header.stamp;
-
 
     if (tfl_->waitForTransform(p_target_frame_, cloud_in->header.frame_id, cloud_in->header.stamp, wait_duration_)){
       tf::StampedTransform transform;
@@ -112,17 +97,11 @@ public:
         point_cloud2_pub_.publish(cloud2_);
         cloud_agg_.clear();
 
-
-
       }else{
-
 
         pcl::PointCloud<pcl::PointXYZ> pc_tmp;
 
-
-
         pcl::fromROSMsg(*cloud_in, pc_tmp);
-
 
         Eigen::Matrix4f sensorToWorld;
         pcl_ros::transformAsMatrix(transform, sensorToWorld);
@@ -136,25 +115,11 @@ public:
       }
 
       prior_roll_angle_ = roll;
-
-    }
-
-    /*
-    if (p_use_high_fidelity_projection_){
-
-      ros::Time end_time   = scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment) ;
-
-      if(tfl_->waitForTransform(p_target_frame_, scan_in->header.frame_id, scan_in->header.stamp, wait_duration_) &&
-         tfl_->waitForTransform(p_target_frame_, scan_in->header.frame_id, end_time, wait_duration_)){
-        projector_.transformLaserScanToPointCloud(p_target_frame_, *scan_to_convert, cloud2_, *tfl_, p_max_range_, laser_geometry::channel_option::Intensity);
-      }else{
-        ROS_WARN("Timed out waiting for transform between %s and %s for %f seconds. Unable to transform laser scan.",p_target_frame_.c_str(), scan_in->header.frame_id.c_str(), wait_duration_.toSec());
-        return;
-      }
     }else{
-      projector_.projectLaser(*scan_to_convert, cloud2_, p_max_range_, laser_geometry::channel_option::Intensity);
+      ROS_ERROR_THROTTLE(5.0, "Cannot transform from sensor %s to target %s . This message is throttled.",
+                         cloud_in->header.frame_id.c_str(),
+                         p_target_frame_.c_str());
     }
-    */
   }
 
 protected:
@@ -164,21 +129,12 @@ protected:
   boost::shared_ptr<tf::TransformListener> tfl_;
   ros::Duration wait_duration_;
 
-  //double p_max_range_;
-  //double p_min_range_;
   bool p_use_high_fidelity_projection_;
   std::string p_target_frame_;
 
   double prior_roll_angle_;
 
-  //laser_geometry::LaserProjection projector_;
-
   sensor_msgs::PointCloud2 cloud2_;
-  //sensor_msgs::LaserScan scan_min_range_;
-
-  //sensor_msgs::LaserScan scan_filtered_;
-  
-  //filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
 
   std::vector<boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> > > cloud_agg_;
 };
@@ -187,7 +143,7 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "vigir_laserscan_to_pointcloud_node");
 
-  LaserscanToPointcloud ls;
+  RotatingCloudToAggregatedCloud ls;
 
   ros::spin();
 }
